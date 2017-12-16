b622d120e94689b1fb2c03ab5c2d28c99b74b2a5
Yuanxin Li <yuanxinli@Yuanxins-MacBook-Pro.local>
2017-12-16 22:23:02 +0800

    3

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express    = require('express');
 var bodyParser = require('body-parser');
 var app        = express();
 var morgan     = require('morgan');
-var default     = express.Router();
+var defaultRoute     = express.Router();
 
 // call the routes required
 const person = require('./js/routes/person');
@@ -22,12 +22,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // DEFAULT ROUTE
-default.get('/', function(req, res) {
+defaultRoute.get('/', function(req, res) {
     res.json({ message: 'Welcome to foodin APIs, have a nice day!' });
 });
 
 // REGISTER OUR ROUTES -------------------------------
-app.use('', default);
+app.use('', defaultRoute);
 app.use('/person', person);
 
 // START THE SERVER
