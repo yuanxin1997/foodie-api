17c4838cda1cc78e30d85428792cd213b9534715
Yuanxin Li <yuanxinli@Yuanxins-MacBook-Pro.local>
2017-12-16 22:21:45 +0800

    3

--- app.js
+++ app.js
@@ -6,32 +6,28 @@ var express    = require('express');
 var bodyParser = require('body-parser');
 var app        = express();
 var morgan     = require('morgan');
-var router     = express.Router();
+var default     = express.Router();
 
+// call the routes required
 const person = require('./js/routes/person');
 
-// configure app
-app.use(morgan('dev')); // log requests to the console
+// set our port
+var port = process.env.PORT || 8080;
+
+// log requests to the console
+app.use(morgan('dev'));
 
 // configure body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080; // set our port
-
-
-// DEFAULT
-router.get('/', function(req, res) {
-    res.json({ message: 'Welcome to our foodin APIs, have a nice day!' });
+// DEFAULT ROUTE
+default.get('/', function(req, res) {
+    res.json({ message: 'Welcome to foodin APIs, have a nice day!' });
 });
 
-// more routes for our API will happen here
-
 // REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('', router);
-
-// REGISTER OUR ROUTES -------------------------------
+app.use('', default);
 app.use('/person', person);
 
 // START THE SERVER
