6c88607ea8a2ecdfc8beb56dba28b2c05c3386b1
Yuanxin Li <yuanxinli@Yuanxins-MacBook-Pro.local>
2018-01-07 20:08:57 +0800

    g

--- js/routes/person.js
+++ js/routes/person.js
@@ -312,7 +312,7 @@ router.get('/getIllnessIndicator/:id', function(req, res)  {
   var id = req.params.id;
   // SQL QUERY
   var request = new Request(
-    "select i.id, ii.name, ii.maxValue, ii.minValue "
+    "select ii.name, ii.maxValue, ii.minValue "
 + "from illnessIndicator ii "
 + "inner join illness i on ii.illnessId = i.id "
 + "inner join personIllness pi on i.id = pi.illnessId "
